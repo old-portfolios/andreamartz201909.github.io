2c76e3de5806b397b843011638d3bf7721b2e61e
Andrea Martz <14165788+wahmof2@users.noreply.github.com>
2019-09-29 09:10:19 -0500

    Customize navbar

--- js/script.js
+++ js/script.js
@@ -15,6 +15,16 @@ $(document).ready(function() {
   });
 
 
+  $("navigation li a").click(function(e) {
+    e.preventDefault();
+
+    const targetElement = $(this).attr("href");
+    const targetPosition = $(targetElement).offset().top;
+    $("html, body").animate({ scrollTop: targetPosition - 50 }, "slow");
+  });
+
+
+
 
   const nav = $("#navigation");
   const navTop = nav.offset().top;
